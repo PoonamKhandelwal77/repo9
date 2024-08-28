763bab7085189a43e9d240fea978ff3bcd25baaa
PoonamKhandelwal77 <poonamkh01@gmail.com>
2024-08-28 15:15:04 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express()
+const port=8800
+
+app.get('/',(req,res)=>{
+    res.send("hello from poonam")
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+    
+})
