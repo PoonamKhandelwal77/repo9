c6df41caff0583d97934f45aa4c5b6f20ca7a195
PoonamKhandelwal77 <poonamkh01@gmail.com>
2024-08-28 15:40:10 +0530

    user route added

--- api/src/db/conn/mConn.js
+++ api/src/db/conn/mConn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const conn=mongoose.connect('mongodb://127.0.0.1:27017/MyDB').then(()=>{
+const conn=mongoose.connect('mongodb+srv://poonam:1234@cluster0.z1dln.mongodb.net/Sshopee').then(()=>{
     console.log("database connected");
 }).catch((e)=>{
     console.log("the error is :========================="+e);
